e79f0e93b0c339c71499aef0900cec766489bb66
Arunkumar1992 <arunkumareee7@gmail.com>
2016-12-08 17:18:15 +0530

    Update app.js

--- app.js
+++ app.js
@@ -29,7 +29,16 @@ app.get('/', routes.index);
 app.get('/hero/:name', routes.hero);
 app.post('/hero/add-fact', routes.addFact);
 app.get('/hero/:name', routes.hero);
-
+  app.get('/webhook', function(req, res) {
+  if (req.query['hub.mode'] === 'subscribe' &&
+      req.query['hub.verify_token'] === 'verifybotapp') {
+    console.log("Validating webhook");
+    res.status(200).send(req.query['hub.challenge']);
+  } else {
+    console.error("Failed validation. Make sure the validation tokens match.");
+    res.sendStatus(403);          
+  }  
+});
 
 app.post('/webhook', function (req, res) {
   var data = req.body;
